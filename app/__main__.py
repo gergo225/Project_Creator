9321cf7c1a367608552dd014bf479f6479272e82
gergo225 <fazekasgergo225@gmail.com>
2020-09-03 16:00:00 +0300

    Create project folder
    Folder is created in the directory where the command is invoked

--- app/__main__.py
+++ app/__main__.py
@@ -1,8 +1,31 @@
 """ Main file """
 
+import os
+import sys
 import click
 
 project_types = ["Python", "Flutter"]
+
+
+def create_folder(name):
+    """Create a new folder for the project in the current directory
+
+    Params
+    ------
+    name : str
+        Name of the project and folder to create
+    """
+    click.echo("Creating project folder...")
+    path = os.path.join(os.getcwd(), name)
+
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        click.echo("Error: Project folder with same name already exits!")
+        click.echo("exiting...")
+        sys.exit()
+    click.echo("-> Folder created")
+
 
 @click.command()
 @click.argument("name")
@@ -23,17 +46,19 @@ project_types = ["Python", "Flutter"]
 def create_project(name, public, desc, project_type):
     """ Creates a new project """
     click.echo(f"Creating project: {name}")
+    create_folder(name)
+
+    if project_type:
+        click.echo(f"Type is: {project_type}")
+    else:
+        click.echo("Empty folder, only Git, no setup")
+
     if public:
         click.echo("Creating PUBLIC repository")
     else:
         click.echo("Creating PRIVATE repository")
     if desc:
         click.echo(f"Description: {desc}")
-    if project_type:
-        click.echo(f"Type is: {project_type}")
-    else:
-        click.echo("Empty folder, only Git, no setup")
-
 
 
 if __name__ == "__main__":
